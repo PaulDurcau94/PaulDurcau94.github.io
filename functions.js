8983ba9ffea2dd4da88b05f13f61fbd97615567a
Durcau Paul <paul.durcau@yahoo.com>
2021-06-24 21:12:57 +0300

    menu update

--- functions.js
+++ functions.js
@@ -14,27 +14,30 @@ console.info(wellcome);
 
 function showHome() {
     document.getElementById('skills').style.display="none";
-    document.getElementById('home').style.display="block";
+    document.getElementById('home').style.display="none";
     document.getElementById('projects').style.display="none";
-    document.getElementById('languages').style.display="none";
+    document.getElementById('home').style.display="block";
+}
 
     
 function showSkills() {
-    document.getElementById('skills').style.display="block";
+    document.getElementById('languages').style.display="none";
     document.getElementById('home').style.display="none";
     document.getElementById('projects').style.display="none";
-    document.getElementById('languages').style.display="none";
+    document.getElementById('skills').style.display="block";
 }
 
 
 function showProjects() {
     document.getElementById('skills').style.display="none";
     document.getElementById('home').style.display="none";
-    document.getElementById('projects').style.display="block";
     document.getElementById('languages').style.display="none";
+    document.getElementById('projects').style.display="block";
+}
 
 function showLanguages() {
     document.getElementById('skills').style.display="none";
     document.getElementById('home').style.display="none";
     document.getElementById('projects').style.display="none";
     document.getElementById('languages').style.display="block";
+}
