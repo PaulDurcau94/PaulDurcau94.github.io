50b4310fdb52836abcf9c15813b58c2e94b9b9fa
Durcau Paul <paul.durcau@yahoo.com>
2021-06-24 21:43:14 +0300

    hidePage function

--- functions.js
+++ functions.js
@@ -1,24 +1,27 @@
+function hidePage(id) {
+    document.getElementById(id).style.display="none";
+}
 
 function showHome() {
-    document.getElementById('skills').style.display="none";
-    document.getElementById('home').style.display="none";
-    document.getElementById('projects').style.display="none";
+    hidePage('skills');
+    hidePage('projects');
+    hidePage('languages');
     document.getElementById('home').style.display="block";
 }
 
     
 function showSkills() {
-    document.getElementById('languages').style.display="none";
-    document.getElementById('home').style.display="none";
-    document.getElementById('projects').style.display="none";
+    hidePage('languages');
+    hidePage('home');
+    hidePage('projects');
     document.getElementById('skills').style.display="block";
 }
 
 
 function showProjects() {
-    document.getElementById('skills').style.display="none";
-    document.getElementById('home').style.display="none";
-    document.getElementById('languages').style.display="none";
+    hidePage('skills');
+    hidePage('home');
+    hidePage('languages');
     document.getElementById('projects').style.display="block";
 }
 
